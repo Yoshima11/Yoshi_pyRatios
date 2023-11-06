615b903b8d53435c9d012f6f1747cc673648bbaa
Yoshima11 <miked160581@gmail.com>
2023-11-05 21:56:14 -0300

    quito web_browser

--- pyRatios.py
+++ pyRatios.py
@@ -85,4 +85,5 @@ def main(page: ft.Page):
     page.add(
         contenedor,
     )
-ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+ft.app(target=main)
+#ft.app(target=main, view=ft.AppView.WEB_BROWSER)
